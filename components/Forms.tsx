1b69c92ed035b2c67e067281e53099eb6404d986
EliasJesusRangel <eli.rangel@atscale.com>
2024-01-10 13:16:35 -0500

    update gitignore

--- components/Forms.tsx
+++ components/Forms.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Link from "next/link";
 import { List } from "./habits/List";
 import { useZustandFormStore } from "@/state/form";
@@ -14,57 +13,19 @@ import {
   navigationMenuTriggerStyle,
 } from "./ui/navigation-menu";
 
-import ListItem from "./ui/list-item";
-const links = [
+import { ListItem } from "./ui/list-item";
+const components = [
   {
-    value: "Add Habit Entry Form",
-    link: "/AddHabitEntryForm",
+    title: "Add Habit Entry Form",
+    href: "/AddHabitEntryForm",
     description: "Add an entry to a habit",
   },
   {
-    value: "Add Habit Form",
-    link: "/AddHabitForm",
+    title: "Add Habit Form",
+    href: "/AddHabitForm",
     description: "Add a habit to the database",
   },
 ];
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
-
 export default function Forms() {
   const { habits } = useZustandFormStore();
 
